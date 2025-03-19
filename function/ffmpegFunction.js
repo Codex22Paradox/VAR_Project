734bf9169ad97a71b0d228f092a70be086ad641d
Stefano Marocco <114689671+FapaKslapa@users.noreply.github.com>
2025-03-19 11:44:32 +0100

    Bug fix

--- function/ffmpegFunction.js
+++ function/ffmpegFunction.js
@@ -28,32 +28,36 @@ ffmpeg.setFfmpegPath(ffmpegStatic);
 export const ffmpegModule = {
     startRecording: async function () {
         try {
-            await new Promise((resolve, reject) => {
-                ffmpeg()
-                    .input(VIDEO_DEVICE)
-                    .inputFormat(isWindows ? 'dshow' : 'v4l2')
-                    .videoCodec('libx264')
-                    .videoBitrate(8000) // Bitrate impostato a 8 Mbps
-                    .outputOptions([
-                        '-preset ultrafast',
-                        '-tune zerolatency',
-                        '-profile:v baseline',
-                        '-level 3.0',
-                        '-pix_fmt yuv420p'
-                    ])
-                    .duration(BUFFER_DURATION)
-                    .on('error', (err) => {
-                        console.error('Errore nell\'avvio di FFmpeg:', err);
-                        reject(err);
-                    })
-                    .on('end', () => {
-                        console.log('FFmpeg ha terminato la registrazione nel buffer.');
-                        resolve();
-                    })
-                    .save(BUFFER_FILE);
-            });
+            const recordBuffer = () => {
+                return new Promise((resolve, reject) => {
+                    ffmpeg()
+                        .input(VIDEO_DEVICE)
+                        .inputFormat(isWindows ? 'dshow' : 'v4l2')
+                        .videoCodec('libx264')
+                        .videoBitrate(8000) // Bitrate impostato a 8 Mbps
+                        .outputOptions([
+                            '-preset ultrafast',
+                            '-tune zerolatency',
+                            '-profile:v baseline',
+                            '-level 3.0',
+                            '-pix_fmt yuv420p'
+                        ])
+                        .duration(BUFFER_DURATION)
+                        .on('error', (err) => {
+                            console.error('Errore nell\'avvio di FFmpeg:', err);
+                            reject(err);
+                        })
+                        .on('end', () => {
+                            console.log('FFmpeg ha terminato la registrazione nel buffer.');
+                            resolve();
+                        })
+                        .save(BUFFER_FILE);
+                });
+            };
 
-            console.log(`FFmpeg sta registrando in RAM con dispositivo: ${VIDEO_DEVICE}`);
+            while (true) {
+                await recordBuffer();
+            }
         } catch (err) {
             console.error('Errore durante la registrazione:', err);
         }
